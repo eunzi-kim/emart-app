17946d9acdd87d1fd4998c13dd4da437b22a8ffe
김은지 <eunzi@gim-eunjiui-MacBookAir.local>
2022-08-30 20:29:00 +0900

    Feat: 위로 가기 버튼

--- src/pages/Advertisement.js
+++ src/pages/Advertisement.js
@@ -14,12 +14,20 @@ function Advertisement() {
     setCategoryName(category);
   };
 
+  // 위로 가기 버튼
+  const onGoUp = () => {
+    window.scrollTo(0, 0);
+  };
+
   return (
     <div className="advertisment">
       <Navbar />
       <div id="ID_INFORMATION">금주의 전단 상품을 만나보세요</div>
       <Menu sendCategory={getCategory} />
       <Main categoryName={categoryName} />
+      <button id="ID_BTN_UP" onClick={onGoUp}>
+        ⬆️
+      </button>
     </div>
   );
 }
